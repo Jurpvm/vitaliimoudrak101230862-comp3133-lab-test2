968e69c4e2b4916d0c0946a95e0c2e8f1148fdb5
Vitalii Moudrak <101230862@georgebrown.ca>
2021-04-09 21:43:34 -0400

    still no

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    CommonModule,
+    RouterModule,
     BrowserModule,
     AppRoutingModule
   ],
